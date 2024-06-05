d97770be34298719742332de909a7cd9118f0b27
Paarcival <sdfranco.19@est.ucab.edu.ve>
2024-06-05 10:51:57 -0400

    Modificaciones a los scripts de Animacion

---
+++ Scripts/Animaciones de navegacion.js
@@ -0,0 +1,12 @@
+document.querySelector('#servicios-scroll').addEventListener('click',(event) => {
+    document.querySelector('#servicios').scrollIntoView({behavior : 'smooth'})    
+})
+
+document.querySelector('#nosotros-scroll').addEventListener('click', even => {
+    document.querySelector('#nosotros').scrollIntoView({behavior : 'smooth'})
+})
+
+document.querySelector('#contacto-scroll').addEventListener('click', event =>{
+    document.querySelector('.contact-form').scrollIntoView({behavior : 'smooth'})
+    document.querySelector('#nombre').focus()
+})
